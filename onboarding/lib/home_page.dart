7af0da36a369892300c1c7ddce34f2f2a3ebcfc6
jueunhong <85168145+jueunhong@users.noreply.github.com>
2022-12-27 23:35:04 +0900

    introductionScreen 패키지 사용해서 온보딩 페이지 만들기

---
+++ onboarding/lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page"),
+      ),
+      body: Center(
+          child: const Text(
+        "환영합니다!",
+        style: TextStyle(
+          fontSize: 24,
+        ),
+      )),
+    );
+  }
+}
---
+++ onboarding/lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+import 'package:onboarding/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: OnboardingPage(),
+    );
+  }
+}
+
+class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: IntroductionScreen(
+      pages: [
+        PageViewModel(
+          title: "빠른 개발",
+          body: "Flutter의 hot reload는 쉽고 UI 빌드를 도와줍니다.",
+          image: Padding(
+            padding: EdgeInsets.all(32),
+            child: Image.network(
+                'https://user-images.githubusercontent.com/26322627/143761841-ba5c8fa6-af01-4740-81b8-b8ff23d40253.png'),
+          ),
+          decoration: PageDecoration(
+            titleTextStyle: TextStyle(
+              color: Colors.blueAccent,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+            bodyTextStyle: TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+            ),
+          ),
+        ),
+        // 두 번째 페이지
+        PageViewModel(
+          title: "표현력 있고 유연한 UI",
+          body: "Flutter에 내장된 아름다운 위젯들로 사용자를 기쁘게 하세요.",
+          image: Image.network(
+              'https://user-images.githubusercontent.com/26322627/143762620-8cc627ce-62b5-426b-bc81-a8f578e8549c.png'),
+          decoration: PageDecoration(
+            titleTextStyle: TextStyle(
+              color: Colors.blueAccent,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+            bodyTextStyle: TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+            ),
+          ),
+        ),
+      ],
+      next: const Text("Next"),
+      done: const Text("Done"),
+      onDone: () {
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => HomePage()));
+      },
+    ));
+  }
+}
